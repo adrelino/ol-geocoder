849c8bf75ed4d48b3a9d9a8c97d3bc2a0a87b3ec
Adrian Haarbach <mail@adrian-haarbach.de>
2023-08-21 16:53:54 +0200

    new nominatim url

--- dist/ol-geocoder.mjs
+++ dist/ol-geocoder.mjs
@@ -463,7 +463,7 @@ class OpenStreet {
    */
   constructor() {
     this.settings = {
-      url: 'https://nominatim.openstreetmap.org/search/',
+      url: 'https://nominatim.openstreetmap.org/search',
 
       params: {
         q: '',
